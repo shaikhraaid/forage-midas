037858528e6760b7f084a73545676519519a2da3
shaikh <shaikh.raaid@utdallas.edu>
2026-06-08 18:43:19 -0500

    Task 3 Check 3

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -4,7 +4,7 @@ package com.jpmc.midascore.component;
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
-import com.jpmc.midascore.repository.TransactionRepository;
+import com.jpmc.midascore.entity.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
