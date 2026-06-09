0e53325c976b14d1c9216e55e39c256ef9401eba
shaikh <shaikh.raaid@utdallas.edu>
2026-06-09 02:35:31 -0500

    Task 1, 2, 3 completed

--- src/main/java/com/jpmc/midascore/repository/TransactionRepository.java
+++ src/main/java/com/jpmc/midascore/repository/TransactionRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionRepository extends CrudRepository<TransactionRecord, Long> {}
+public interface TransactionRepository extends CrudRepository<TransactionRecord, Long> {
+}
